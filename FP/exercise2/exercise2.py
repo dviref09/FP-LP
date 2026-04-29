3aad5a20b49f22826f2b7fd80617fe2b7a6cf192
Dvir Farkash <dviref09@gmail.com>
2026-04-29 23:55:38 +0300

    finishing question 1

--- FP/exercise2/exercise2.py
+++ FP/exercise2/exercise2.py
@@ -5,16 +5,16 @@ def pentaNumRange(n1, n2):
 
 def unfunctionalPrint():
     try:
-        n1 = int(input("please enter two number n1, n2:\n"))
-        n2 = int(input())
+        n1 = int(input("enter the value of n1: "))
+        n2 = int(input("enter the value of n1: "))
     except ValueError:
         print("ERROR: the input should be integers")
         return
     if (n1 < 1 or n2 < 1):
-        print("ERROR: the numbers should be positive")
+        print("ERROR: the values must be positive integers and n2 > n1")
         return
     if (n1 >= n2):
-        print("ERROR: n2 should be bigger than n1")
+        print("ERROR: the values must be positive integers and n2 > n1")
         
     numbers = pentaNumRange(n1, n2)
     for i, num in enumerate(numbers):
@@ -22,19 +22,45 @@ def unfunctionalPrint():
             print()
         print(num, end = " ")
     print()
-        
-def functoinal(n1, n2):
-    pass
-
+    
+# this is the functional function in question 1
+def functional1(n1, n2):
+    if (n1 >= n2 or n1 < 1 or n2 < 1):
+        return "ERROR: the values must be positive integers and n2 > n1"
+    
+    # gets the numbers
+    numbers = pentaNumRange(n1, n2)
+    
+    str_numbers = list(map(str, numbers))
+    
+    if (n2 - n1 <= 10):
+        return " ".join(str_numbers[:])
+    else: 
+        # every 10 numbers inserts a '\n'
+        return " ".join(str_numbers[0 : 10]) + "\n" + functional1(n1 + 10, n2)
+    
+# this is unfunctional function for the I/O part
 def question1():
-    pass
+    try:
+        n1 = int(input("enter the value of n1: "))
+        n2 = int(input("enter the value of n2: "))
+    except ValueError:
+        print("ERROR: the input should be integers")
+        return
+    if (n1 < 1 or n2 < 1):
+        print("ERROR: the values must be positive integers and n2 > n1")
+        return
+    if (n1 >= n2):
+        print("ERROR: the values must be positive integers and n2 > n1")
+        
+    print(functional1(n1, n2))
     
         
 
 # running
 def main():
-    lfuncs = []
-    lstrs = ["exit"]
+    lfuncs = [question1]
+    lstrs = ["exit", "penta numbers"]
     
     while True:
         print("your choices:")
@@ -49,5 +75,4 @@ def main():
             print ("error")
    
 if __name__ == "__main__":
-    unfunctionalPrint()
     main()
---
+++ FP/exercise2/main.py
@@ -0,0 +1,4 @@
+arr = [1, 2, 3, 4, 5, 6, 7]
+arr = list(map(str, arr))
+
+print("".join(arr[0:10]))
