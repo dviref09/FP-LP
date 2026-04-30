50d114535228dceaeab25e2cbe1ca5aa3ec9683c
Dvir Farkash <dviref09@gmail.com>
2026-04-30 20:12:11 +0300

    finished question4

--- FP/exercise2/exercise2.py
+++ FP/exercise2/exercise2.py
@@ -1,3 +1,4 @@
+# Dvir Farkash 333228062
 # question 1
 def pentaNumRange(n1, n2):
     getPentaNum = lambda n: 0.5 * n * (3*n - 1)
@@ -55,12 +56,54 @@ def question1():
         
     print(functional1(n1, n2))
     
-        
+# question 2
+def sumDigits(n):
+    def gettingDigits(n):
+        if (n < 10):
+            return [n]
+        else:
+            return [n % 10] + gettingDigits(n // 10)
+    return sum(gettingDigits(abs(n)))
 
+def question2():
+    try:
+        n = int(input("Enter an integer number n (positive or negative): "))
+        print(sumDigits)
+    except (ValueError):
+        print("ERROR: Input number is incorrect!")
+    
+# question 3
+def isPalindrome(n):
+    reverseNumber = lambda n: int(str(n)[::-1])
+    return n == reverseNumber(n)
+
+def question3():
+    try:
+        n = int(input("Enter an integer number n (positive or negative): "))
+        print("It is a palindrome" if isPalindrome(n) else "It is not a palindrome")
+    except (ValueError):
+        print("ERROR: Input number is incorrect!")
+
+# question 4
+def m(n):
+    def firstNTerms(n):
+        iTerm = lambda i: i / (i + 1)
+        return list(map(iTerm, range(1, n + 1)))
+    return sum(firstNTerms(n))
+
+def question4():
+    try:
+        n = int(input("Enter a Natural number n: "))
+        if (n < 1):
+            raise ValueError
+        print("\n".join(map(lambda n: f"{n} {m(n)}", range(1, n + 1))))
+    except ValueError:
+        print("ERROR: Input number is incorrect!")
+    
 # running
 def main():
-    lfuncs = [question1]
-    lstrs = ["exit", "penta numbers"]
+    lfuncs = [question1, question2, question3, question4]
+    lstrs = ["exit", "penta numbers", "sum digits", "palindrome check", "series claculation"]
     
     while True:
         print("your choices:")
