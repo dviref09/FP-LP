525bd1d9645f04f81e1c0b3a811d17b171d112c6
Dvir Farkash <dviref09@gmail.com>
2026-06-21 02:45:30 +0300

    middle of exercise 3

--- LP/exercise3/exercise3.pl
+++ LP/exercise3/exercise3.pl
@@ -1,9 +1,19 @@
-scum_helper(1,1):-!.
-scum_helper(N,Res):-N1 is N - 1, scum_helper(N1, Res1), Res is Res1 + N.
-scum(N,Res):-(integer(N), N > 1) -> scum_helper(N,Res); (write("invalid input"), nl, fail).
+scum(1,1):-!.
+scum(N,_):- \+integer(N); N < 1, !, write("Invalid input"), nl, fail.
+scum(N, Sum):- N1 is N - 1, scum(N1, RestSum), Sum is RestSum + N.
+
 
 sumDigits(0, 0):-!.
-sumDigits(Num, Sum):-integer(Num), Num >= 0, Digit is Num mod 10, Rest is Num // 10, sumDigits(Rest, RestSum), Sum is RestSum + Digit.
+sumDigits(N,_):- \+integer(N); N < 0, !, write("Invalid input"), nl, fail.
+sumDigits(Num, Sum):-Digit is Num mod 10, Rest is Num // 10, sumDigits(Rest, RestSum), Sum is RestSum + Digit.
 
+split(N,_):- \+integer(N); N < 0, !, write("Invalid input"), nl, fail.
 split(N, [N]):-integer(N), N >= 0, N < 10,!.
-split(N, Res):-(integer(N), N > 0, Digit is N mod 10, Rest is N // 10, split(Rest, RestRes), append(RestRes, [Digit], Res)).
+split(N, Res):-Digit is N mod 10, Rest is N // 10, split(Rest, RestRes), append(RestRes, [Digit], Res).
+
+create(List, _):- \+is_list(List), !, write("Invalid input"), nl, fail.
+create([N], N):- integer(N), N >= 0, N < 10, !.
+create([Head|Rest], N):- \+integer(Head); Head < 0, !, write("Invalid input"), nl, fail.
+create([Head|Rest], N):- create(Rest, RestN), N is RestN * 10 + Head.
+
+reverseNum(N, Rev):- split(N, Digits), create(Digits, Rev).
